cfcc9ce7bf5165839bbab362b8f0d40bf65b9f65
Menaka Godakanda <156875412+MenakaGodakanda@users.noreply.github.com>
2024-07-10 15:44:24 +0800

    Create basic_firewall.sh

---
+++ scripts/basic_firewall.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Flush existing rules
+iptables -F
+iptables -X
+
+# Default policies
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# Allow loopback traffic
+iptables -A INPUT -i lo -j ACCEPT
+
+# Allow established connections
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+
+# Allow SSH
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Allow HTTP and HTTPS
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+
+echo "Basic firewall rules configured."
